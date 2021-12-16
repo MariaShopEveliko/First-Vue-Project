72b6bb48ef8f79e9340545617d9103183d6c4b07
Maria Filipova <maria.shop@eveliko.com>
2021-12-16 17:10:59 +0200

    Section 3: Lecture

--- Lecture/3 - lists-cond-01-starting-setup/app.js
+++ Lecture/3 - lists-cond-01-starting-setup/app.js
@@ -1,7 +1,19 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { 
+      goalVal:"",
+      goals: [],
+      myObj: {name: 'Max', age: 31}
+    };
   },
+  methods:{
+    addGoal(){
+      this.goals.push(this.goalVal);
+    },
+    removeGoal(index){
+      this.goals.splice(index, 1);
+    }
+  }
 });
 
 app.mount('#user-goals');
