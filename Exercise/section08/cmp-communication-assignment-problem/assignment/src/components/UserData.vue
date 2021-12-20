95cb6aac043e512c7dacbe9cbf7895898b471b21
Maria Filipova <maria.shop@eveliko.com>
2021-12-20 15:53:51 +0200

    Assignment 1

---
+++ Exercise/section08/cmp-communication-assignment-problem/assignment/src/components/UserData.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="submitUser">
+    <div>
+      <label>Name:</label>
+      <input type="text" v-model="enteredName"/>
+    </div>
+    <div>
+      <label>Age:</label>
+      <input type="number"  v-model="enteredAge"/>
+    </div>
+    <button>Add User</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["submit-user"],
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: "",
+    };
+  },
+  methods: {
+    submitUser() {
+      this.$emit("submit-user", this.enteredName, this.enteredAge);
+    },
+  },
+};
+</script>
