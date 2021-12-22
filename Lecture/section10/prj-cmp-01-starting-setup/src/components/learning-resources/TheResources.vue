92c52436019728b908627c889bdb7af6c3d3e624
Maria Filipova <maria.shop@eveliko.com>
2021-12-22 10:13:56 +0200

    Section 10 - Lecture: Stored resources tab

---
+++ Lecture/section10/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,62 @@
+<template>
+  <base-card>
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResourcesMode">
+      Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resource')"
+      :mode="addResourceMode">
+        Add Resource
+    </base-button>
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: {
+    StoredResources, // "stored-resources": StoredResources
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+    };
+  },
+  computed:{
+      storedResourcesMode(){
+          return this.selectedTab === 'stored-resources' ? null : 'flat';
+      },
+      addResourceMode(){
+          return this.selectedTab === 'add-resource' ? null : 'flat';
+      }
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
