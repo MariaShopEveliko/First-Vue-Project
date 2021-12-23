54ee3c71e5e1f45bddec6f7c94c07ebdce17d972
Maria Filipova <maria.shop@eveliko.com>
2021-12-23 13:23:35 +0200

    Register routes

--- Lecture/section13/routing-01-starting-setup/src/main.js
+++ Lecture/section13/routing-01-starting-setup/src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router'; // npm install --save vue-router@next
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 
-const app = createApp(App)
+const router = createRouter({
+    history: createWebHistory(), // to be able to go back
+    routes: [ //urls
+        { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
+        { path: '/users', component: UsersList }
+    ]
+});
+const app = createApp(App);
 
+app.use(router);
 app.mount('#app');
