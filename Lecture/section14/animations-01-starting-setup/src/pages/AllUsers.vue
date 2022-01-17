a87b462332bc7d0af05427c877167d1577d0c618
Maria Filipova <maria.shop@eveliko.com>
2022-01-17 17:19:44 +0200

    Added new pages

---
+++ Lecture/section14/animations-01-starting-setup/src/pages/AllUsers.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container">
+        <h2>All users</h2>
+        <router-link to="/">Home</router-link>
+        <router-link to="/goals">Course goals</router-link>
+    </div>
+</template>
---
+++ Lecture/section14/animations-01-starting-setup/src/pages/CourseGoals.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container">
+        <h2>Course goals</h2>
+        <router-link to="/">Home</router-link>
+        <router-link to="/users">All users</router-link>
+    </div>
+</template>
