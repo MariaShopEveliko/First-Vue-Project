f65f54904a25a9a2883f2f8a0f0bbeb026e5ba04
Maria Filipova <maria.shop@eveliko.com>
2022-01-20 00:01:37 +0200

    Coaches list, detail & filter

--- Lecture/section16/main-prj-01-starting-setup/src/router.js
+++ Lecture/section16/main-prj-01-starting-setup/src/router.js
@@ -14,6 +14,7 @@ const router = createRouter({
         { path: '/coaches', component: CoachesList },
         {
             path: '/coaches/:id', component: CoachDetail,
+            props: true,
             children: [
                 { path: '/contact', component: ContactCoach }, // /coaches/coachId/contac
             ]
